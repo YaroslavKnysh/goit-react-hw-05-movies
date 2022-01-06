7d659494c8791fcb8637f826b6c63920133bf600
YaroslavKnysh <yarik.knysh@gmail.com>
2022-01-06 19:31:02 +0200

    стили

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
+import './App.css';
 const HomePage = lazy(() => import('./components/HomePage'));
 const MoviePage = lazy(() => import('./components/MoviesPage'));
 const MovieDetailsPage = lazy(() => import('./components/MovieDetailsPage'));
@@ -8,25 +9,29 @@ const Reviews = lazy(() => import('./components/Reviews'));
 
 function App() {
   return (
-    <div className="App">
-      <header>
+    <div>
+      <header className="header">
         <nav>
-          <NavLink exact="true" to="/">
+          <NavLink className="navLink_home" exact="true" to="/">
             Home
           </NavLink>
-          <NavLink to="/movies">Movies</NavLink>
+          <NavLink className="navLink_movies" to="/movies">
+            Movies
+          </NavLink>
         </nav>
       </header>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <Routes>
-          <Route path="/" exact="true" element={<HomePage />} />
-          <Route path="/movies" element={<MoviePage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <div className="App">
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Routes>
+            <Route path="/" exact="true" element={<HomePage />} />
+            <Route path="/movies" element={<MoviePage />} />
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="/movies/:movieId/cast" element={<Cast />} />
+              <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </div>
     </div>
   );
 }
--- src/components/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, useParams, Outlet, useNavigate } from 'react-router-dom';
 import * as GetApi from './Service';
+import s from './style/MovieDetails.module.css';
 
 export default function MovieDetailsPage() {
   const [movie, setMovie] = useState(null);
@@ -33,9 +34,19 @@ export default function MovieDetailsPage() {
       <p>Overview: {movie.overview}</p>
       <p>Genres: {movie.genres.map(genre => genre.name).join(', ')}</p>
       <a href={`${movie.homepage}`}>Movie Homepage</a>
-      <div>
-        <NavLink to={`/movies/${movie.id}/cast`}>Cast</NavLink>
-        <NavLink to={`/movies/${movie.id}/reviews`}>Reviews</NavLink>
+      <div className={s.movieDetails_linkContainer}>
+        <NavLink
+          className={s.movieDetails_link_cast}
+          to={`/movies/${movie.id}/cast`}
+        >
+          Cast
+        </NavLink>
+        <NavLink
+          className={s.movieDetails_link_reviews}
+          to={`/movies/${movie.id}/reviews`}
+        >
+          Reviews
+        </NavLink>
       </div>
       <Outlet />
     </div>
